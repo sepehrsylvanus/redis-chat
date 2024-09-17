a2a181f117a7ce60e786a84344554bf3b9ffa0ba
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2024-09-17 21:20:07 +0300

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import ChatLayout from "@/components/chat/ChatLayout";
 import PreferencesTab from "@/components/PreferencesTab";
+import { cookies } from "next/headers";
 
 export default function Home() {
+  const layout = cookies().get("react-resizable-panels:layout");
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
   return (
     <main className="flex h-screen flex-col items-center justify-center p-4 md:px-24 py-32 gap-4">
       <PreferencesTab />
@@ -10,6 +14,10 @@ export default function Home() {
 				dark:bg-[size:20px_20px] bg-[#ffffff] bg-[radial-gradient(#00000033_1px,#ffffff_1px)] bg-[size:20px_20px]"
         aria-hidden="true"
       />
+
+      <div className="z-10 border rounded-lg max-w-5xl w-full min-h-[85vh] text-sm lg:flex">
+        <ChatLayout defaultLayout={defaultLayout} />
+      </div>
     </main>
   );
 }
